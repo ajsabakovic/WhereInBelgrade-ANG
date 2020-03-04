4706549920e8c1c2a258b3837f0c89e62711d9bd
acimovteodora <acimov.teodora@gmail.com>
2020-03-04 17:57:47 +0100

    Jumbotron

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      NavComponent
+      NavComponent,
+      JumbotronComponent
    ],
    imports: [
       BrowserModule,
