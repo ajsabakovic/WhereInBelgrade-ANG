0c14a7c311a2dd49bb39af7af701ab7c64cc64d9
acimovteodora <acimov.teodora@gmail.com>
2020-04-11 18:41:08 +0200

    Covid19 API done

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { count } from 'rxjs/operators';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  covidData: any;
+  serbia: any;
 
   constructor() { }
 
   ngOnInit() {
+    fetch('https://api.covid19api.com/summary')
+    .then(response => response.json())
+    .then(data => {this.setData(data); });
   }
 
+  setData(data: any) {
+    this.covidData = data;
+    const countries = data.Countries;
+    countries.forEach(country => {
+      if (country.CountryCode === 'RS')
+      {
+        this.covidData.UkupnoObolelih = country.TotalConfirmed;
+        this.covidData.UkupnoMrtvih = country.TotalDeaths;
+        this.covidData.UkupnoOporavljenih = country.TotalRecovered;
+      }
+    });
+  }
 }
